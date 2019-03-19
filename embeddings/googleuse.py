1b9c1d554e6e59c623deef7d926eab81b6ab6f33
nstawfik <48722778+nstawfik@users.noreply.github.com>
2019-03-19 20:36:10 +0200

    Update googleuse.py

--- embeddings/googleuse.py
+++ embeddings/googleuse.py
@@ -21,7 +21,7 @@ parser = argparse.ArgumentParser(description='Goggle USE Embeddings')
 parser.add_argument("--data_path", type=str, default='./data', help="Path to data (default ./data)")
 parser.add_argument("--nhid", type=int, default=0, help="number of hidden layers: 0 for Logistic Regression or >0 for MLP (default 0)")
 parser.add_argument('--tasks', nargs='+', default= ['BioC','CitationSA','ClinicalSA','BioASQ','PICO','PUBMED20K','RQE','ClinicalSTS','BIOSSES','MEDNLI'] ,help="Bio Tasks to evaluate (default ALL TASKS)")
-parser.add_argument("--folds", type=int, default=0, help="number of k-folds for cross validations(default 10)")
+parser.add_argument("--folds", type=int, default=10, help="number of k-folds for cross validations(default 10)")
 
 params, _ = parser.parse_known_args()
 # Set PATHs
