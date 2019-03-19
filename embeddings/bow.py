297c07203216015088979cd309de837892218e89
nstawfik <48722778+nstawfik@users.noreply.github.com>
2019-03-19 19:42:21 +0200

    Update bow.py

--- embeddings/bow.py
+++ embeddings/bow.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser(description='Flair Embeddings')
 parser.add_argument("--data_path", type=str, default='./data', help="Path to data (default ./data)")
 parser.add_argument('--embedding_path', type=str, default= './embeddings/glove/glove.840B.300d.txt',help="Path to embeddings (default ./embeddings/glove/glove.840B.300d.txt")
 parser.add_argument("--nhid", type=int, default=0, help="number of hidden layers: 0 for Logistic Regression or >0 for MLP (default 0)")
-parser.add_argument('--tasks', nargs='+', default=['BIOSSES', 'ClinicalSTS', 'PICO' ,'PUBMED20K','RQE','MEDNLI','ClinicalSTS2'] ,help="Bio Tasks to evaluate (default [BIOSSES ClinicalSTS PICO PUBMED20K RQE MEDNLI RQE] )")
+parser.add_argument('--tasks', nargs='+', default= ['BioC','CitationSA','ClinicalSA','BioASQ','PICO','PUBMED20K','RQE','ClinicalSTS','BIOSSES','MEDNLI'] ,help="Bio Tasks to evaluate (default ALL TASKS)")
 parser.add_argument("--folds", type=int, default=0, help="number of k-folds for cross validations(default 10)")
 
 params, _ = parser.parse_known_args()
