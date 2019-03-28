1c45194b9a11b64982585367487013850a5501a6
nstawfik <48722778+nstawfik@users.noreply.github.com>
2019-03-28 12:37:25 +0200

    Update bow.py

--- embeddings/bow.py
+++ embeddings/bow.py
@@ -102,6 +102,7 @@ def get_wordvec(path_to_vec, word2id):
         with io.open(path_to_vec, 'r', encoding='utf-8') as f:
         # if word2vec or fasttext file : skip first line "next(f)"
             for line in f:
+                print(word)
                 word, vec = line.split(' ', 1)
             if word in word2id:
                 print(word,vec)
