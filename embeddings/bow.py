3a4413aa3bdd22b34122a1d99d814690d943dbe5
nstawfik <48722778+nstawfik@users.noreply.github.com>
2019-03-31 09:46:58 +0200

    Update bow.py

--- embeddings/bow.py
+++ embeddings/bow.py
@@ -82,14 +82,16 @@ def get_wordvec(path_to_vec, word2id):
 
     logging.info('Found {0} words with word vectors, out of \
         {1} words'.format(len(word_vec), len(word2id)))
-    return word_vec
+    wvec_dim=(len( np.fromstring(vec, sep=' '))
+    print(word_vec)
+    return word_vec,wvec_dim
 
 
 # SentEval prepare and batcher
 def prepare(params, samples):
     _, params.word2id = create_dictionary(samples)
-    params.word_vec = get_wordvec(PATH_TO_VEC, params.word2id)
-    params.wvec_dim = 300
+    params.word_vec,params.wvec_dim = get_wordvec(PATH_TO_VEC, params.word2id)
+    #params.wvec_dim= 300
     return
 
 def batcher(params, batch):
