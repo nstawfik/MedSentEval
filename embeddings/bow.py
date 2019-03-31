58030f8aefc740cd576b250fd91b547b7aa65f9e
nstawfik <48722778+nstawfik@users.noreply.github.com>
2019-03-31 11:26:54 +0200

    Update bow.py

--- embeddings/bow.py
+++ embeddings/bow.py
@@ -116,10 +116,6 @@ def batcher(params, batch):
     return embeddings
 
 
-# Set params for SentEval
-params_senteval = {'task_path': PATH_TO_DATA, 'usepytorch': True, 'kfold': 5}
-params_senteval['classifier'] = {'nhid': 0, 'optim': 'rmsprop', 'batch_size': 128,
-                                 'tenacity': 3, 'epoch_size': 2}
 
 # Set up logger
 logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.DEBUG)
