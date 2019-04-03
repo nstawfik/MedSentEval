507161078ca6f7995869524918d2bbec3e6a8369
nstawfik <48722778+nstawfik@users.noreply.github.com>
2019-04-03 23:59:22 +0200

    Update bioasq.py

--- senteval/bioasq.py
+++ senteval/bioasq.py
@@ -98,8 +98,8 @@ class BioASQEval(object):
         testC = rqe_embed['test']['chq']
         testF = rqe_embed['test']['faq']
         #testCF = np.c_[testC, testF,  np.abs(testC - testF), testC * testF]
-        testCF = np.hstack((testC, testF, testC * testF,np.abs(testC - testF)))
-        testCF = np.abs(testC - testF)))
+        #testCF = np.hstack((testC, testF, testC * testF,np.abs(testC - testF)))
+        testCF = np.abs(testC - testF)
         
         testY = rqe_embed['test']['label']
 
