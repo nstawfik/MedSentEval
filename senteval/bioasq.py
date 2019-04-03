44e964afbf46d4845b5f4e0e93eec0320dae2b67
nstawfik <48722778+nstawfik@users.noreply.github.com>
2019-04-03 19:26:21 +0200

    Update bioasq.py

--- senteval/bioasq.py
+++ senteval/bioasq.py
@@ -44,6 +44,8 @@ class BioASQEval(object):
         tgt2idx = {'no': 0, 'yes': 1}
         with io.open(fpath, 'r', encoding='utf-8') as f:
             for line in f:
+                print(line)
+                print(line.split('\t'))
                 question, snippet, label = line.strip().split('\t')
                 qa_data['question'].append(question.split(' '))
                 qa_data['snippet'].append(snippet.split(' '))
