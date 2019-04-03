ad332eaf615a8c9c2654a067064dd36deceb8733
nstawfik <48722778+nstawfik@users.noreply.github.com>
2019-04-03 17:16:36 +0200

    Update bioasq.py

--- senteval/bioasq.py
+++ senteval/bioasq.py
@@ -20,14 +20,14 @@ from senteval.tools.validation import KFoldClassifier
 from sklearn.metrics import f1_score
 
 
-class RQEEval(object):
+class BioASQEval(object):
     def __init__(self, task_path, seed=1111):
-        logging.info('***** Transfer task : RQE *****\n\n')
+        logging.info('***** Transfer task : BioASQ task b / Phase b / yes_no quetsions *****\n\n')
         self.seed = seed
         train = self.loadFile(os.path.join(task_path,
-                              'rqe_train.txt'))
+                              'BioASQ_train.txt'))
         test = self.loadFile(os.path.join(task_path,
-                             'rqe_test.txt'))
+                             'BioASQ_test.txt'))
         
         self.rqe_data = {'train': train, 'test': test}
         
@@ -40,21 +40,15 @@ class RQEEval(object):
         return prepare(params, samples)
 
     def loadFile(self, fpath):
-        rqe_data = {'chq': [], 'faq': [], 'label': [],'pid':[]}
-        tgt2idx = {'false': 0, 'true': 1}
+        qa_data = {'question': [], 'snippet': [], 'label': []}
+        tgt2idx = {'no': 0, 'yes': 1}
         with io.open(fpath, 'r', encoding='utf-8') as f:
             for line in f:
-                text = line.strip().split('\t')
-                
-                try:
-                  #print(text[0],"-",text[1],"-",text[2],"-",text[3])
-                  rqe_data['faq'].append(text[3].split(' '))
-                  rqe_data['chq'].append(text[2].split(' '))
-                  rqe_data['label'].append(tgt2idx[text[1]])
-                  rqe_data['pid'].append(text[0])
-                except:
-                  pass
-        return rqe_data
+                question, snippet, label = line.strip().split('\t')
+                qa_data['question'].append(question.split(' '))
+                qa_data['snippet'].append(snippet.split(' '))
+                qa_data['label'].append(tgt2idx[label])
+        return qa_data
 
     def run(self, params, batcher):
         rqe_embed = {'train': {}, 'test': {}}
@@ -63,48 +57,47 @@ class RQEEval(object):
             logging.info('Computing embedding for {0}'.format(key))
             # Sort to reduce padding
             text_data = {}
-            sorted_corpus = sorted(zip(self.rqe_data[key]['chq'],
-                                       self.rqe_data[key]['faq'],
-                                       self.rqe_data[key]['label'],
-                                       self.rqe_data[key]['pid']),
+            sorted_corpus = sorted(zip(self.qa_data[key]['question'],
+                                       self.qa_data[key]['snippet'],
+                                       self.qa_data[key]['label']),
                                    key=lambda z: (len(z[0]), len(z[1]), z[2]))
-            text_data['chq'] = [x for (x, y, z, w) in sorted_corpus]
-            text_data['faq'] = [y for (x, y, z, w) in sorted_corpus]
-            text_data['label'] = [z for (x, y, z, w ) in sorted_corpus]
-            text_data['pid'] = [w for (x, y, z, w ) in sorted_corpus]
-            for txt_type in ['chq', 'faq']:
-                rqe_embed[key][txt_type] = []
+            text_data['question'] = [x for (x, y, z) in sorted_corpus]
+            text_data['snippet'] = [y for (x, y, z) in sorted_corpus]
+            text_data['label'] = [z for (x, y, z) in sorted_corpus]
+            
+            for txt_type in ['question', 'snippet']:
+                qa_embed[key][txt_type] = []
                 for ii in range(0, len(text_data['label']), params.batch_size):
                     batch = text_data[txt_type][ii:ii + params.batch_size]
                     embeddings = batcher(params, batch)
-                    rqe_embed[key][txt_type].append(embeddings)
-                rqe_embed[key][txt_type] = np.vstack(rqe_embed[key][txt_type])
-            rqe_embed[key]['label'] = np.array(text_data['label'])
+                    qa_embed[key][txt_type].append(embeddings)
+                qa_embed[key][txt_type] = np.vstack(qa_embed[key][txt_type])
+            qa_embed[key]['label'] = np.array(text_data['label'])
             logging.info('Computed {0} embeddings'.format(key))
 
         # Train
-        trainC = rqe_embed['train']['chq']
-        trainF = rqe_embed['train']['faq']
-        trainCF = np.c_[np.abs(trainC - trainF), trainC * trainF]
+        trainQ = qa_embed['train']['question']
+        trainS = qa_embed['train']['snippet']
+        trainQS = np.c_[np.abs(trainQ - trainS), trainQ * trainS]
         trainY = rqe_embed['train']['label']
 
         # Test
-        testC = rqe_embed['test']['chq']
-        testF = rqe_embed['test']['faq']
-        testCF = np.c_[np.abs(testC - testF), testC * testF]
+        testQ = rqe_embed['test']['question']
+        testS = rqe_embed['test']['snippet']
+        testQS = np.c_[np.abs(testQ - testS), testQ * testS]
         testY = rqe_embed['test']['label']
 
         config = {'nclasses': 2, 'seed': self.seed,
                   'usepytorch': params.usepytorch,
                   'classifier': params.classifier,
                   'nhid': params.nhid, 'kfold': params.kfold}
-        clf = KFoldClassifier(train={'X': trainCF, 'y': trainY},
-                              test={'X': testCF, 'y': testY}, config=config)
+        clf = KFoldClassifier(train={'X': trainQS, 'y': trainY},
+                              test={'X': testQS, 'y': testY}, config=config)
 
         devacc, testacc, yhat = clf.run()
         testf1 = round(100*f1_score(testY, yhat), 2)
-        logging.debug('Dev acc : {0} Test acc {1}; Test F1 {2} for RQE.\n'
+        logging.debug('Dev acc : {0} Test acc {1}; Test F1 {2} for BioASQ 5b task (yes/no questions).\n'
                       .format(devacc, testacc, testf1))
         return {'devacc': devacc, 'acc': testacc, 'f1': testf1,
-                'ndev': len(trainCF), 'ntest': len(testCF)}
+                'ndev': len(trainQS), 'ntest': len(testQS)}
 
