de0c9f02b11048e10cecc32397476fe098e139dd
nstawfik <48722778+nstawfik@users.noreply.github.com>
2019-04-04 22:06:58 +0200

    Update classifier.py

--- senteval/tools/classifier.py
+++ senteval/tools/classifier.py
@@ -184,7 +184,7 @@ class MLP(PyTorchClassifier):
         self.max_epoch = 200 if "max_epoch" not in params else params["max_epoch"]
         self.dropout = 0. if "dropout" not in params else params["dropout"]
         self.batch_size = 64 if "batch_size" not in params else params["batch_size"]
-        print(params)
+        
         if params["nhid"] == 0:
             self.model = nn.Sequential(
                 nn.Linear(self.inputdim, self.nclasses),
