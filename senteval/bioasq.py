91e739716849d5130194a6ad3259c1473c5b67a0
nstawfik <48722778+nstawfik@users.noreply.github.com>
2019-04-04 13:18:52 +0200

    Update bioasq.py

--- senteval/bioasq.py
+++ senteval/bioasq.py
@@ -108,7 +108,7 @@ class BioASQEval(object):
         config_classifier = copy.deepcopy(params.classifier)
         #config_classifier['max_epoch'] = 15
         config_classifier['epoch_size'] = 1
-        config_classifier['batch_size'] =4
+        config_classifier['batch_size'] =128
         config['classifier'] = config_classifier
         print(config_classifier)
         
