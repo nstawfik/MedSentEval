359258a6a3838e0e3e55d46a5fa1822e7d101ef4
nstawfik <48722778+nstawfik@users.noreply.github.com>
2019-04-09 11:36:09 +0200

    Update elmo.py

--- embeddings/elmo.py
+++ embeddings/elmo.py
@@ -99,10 +99,9 @@ def batcher(params, batch):
        
     #for elmo_embedding in params.elmo.embed_sentences(batch):  
     for elmo_embedding in params_senteval['elmo'].embed_sentences(batch):
-        # Average the 3 layers returned from ELMo #1024
-	avg_elmo_embedding = np.average(elmo_embedding, axis=0)        
+    	avg_elmo_embedding = np.average(elmo_embedding, axis=0)        
         #concatenate the 3 layers returned from ELMo #3072
-        comb_elmo_embedding = np.concatenate(elmo_embedding, axis=1)
+	comb_elmo_embedding = np.concatenate(elmo_embedding, axis=1)
         mowe_elmo=np.mean(comb_elmo_embedding, axis=0) 
         embeddings.append(mowe_elmo)
 	print(elmo_embedding.shape,avg_elmo_embedding.shape,comb_elmo_embedding.shape,mowe_elmo)
