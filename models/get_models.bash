6b2020dcdd8accbbde6b86e33f51a279ed00e6c7
nstawfik <48722778+nstawfik@users.noreply.github.com>
2019-10-17 14:42:43 +0200

    Update get_models.bash

--- models/get_models.bash
+++ models/get_models.bash
@@ -1,1 +1,25 @@
 
+
+wget -Lo glove.840B.300d.zip http://nlp.stanford.edu/data/glove.840B.300d.zip
+wget http://evexdb.org/pmresources/vec-space-models/PubMed-and-PMC-w2v.bin
+unzip glove.840B.300d.zip
+
+
+wget -Lo crawl-300d-2M.vec.zip https://s3-us-west-1.amazonaws.com/fasttext-vectors/crawl-300d-2M.vec.zip
+unzip /content/crawl-300d-2M.vec.zip
+
+
+wget http://www.llwang.net/pubmed_fasttext/pubmed_noncomm_fasttext_model.vec.tar.gz
+tar xvzf pubmed_noncomm_fasttext_model.vec.tar.gz
+
+wget https://storage.googleapis.com/bert_models/2018_10_18/cased_L-12_H-768_A-12.zip
+unzip cased_L-12_H-768_A-12.zip
+
+
+wget https://s3-us-west-2.amazonaws.com/ai2-s2-research/scibert/tensorflow_models/scibert_scivocab_uncased.tar.gz
+tar -xvf scibert_scivocab_uncased.tar.gz
+
+wget infersent2.pkl https://dl.fbaipublicfiles.com/senteval/infersent/infersent1.pkl
+wget infersent2.pkl https://dl.fbaipublicfiles.com/senteval/infersent/infersent2.pkl
+
+
